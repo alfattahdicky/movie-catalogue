9f02adac14c0a5b226d6baf3bb0679324ce33bfe
alfattahdicky <alfattahdicky2@gmail.com>
2021-06-20 18:50:43 +0700

    Service Worker, Cache API

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -6,7 +6,7 @@ const routes = {
   '/': NowPlaying, // default page
   '/now-playing': NowPlaying,
   '/upcoming': UpComing,
-  '/detail:id': Detail
+  '/detail/:id': Detail
 }
 
 export default routes;
